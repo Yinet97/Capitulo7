75de9d65a0f8b87c8a62abdef6eee457568e6fb0
Yinet Jaquez <Yinet Jaquez@YINET>
2016-09-23 20:57:39 -0400

    2 Hacer un programa que funcione como un diccionario, con palabra y definición, usando el Hashtable.

---
+++ Diccionario/Diccionario.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Diccionario
+{
+    partial class Diccionario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Traducir = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Diccionario ";
+            // 
+            // Traducir
+            // 
+            this.Traducir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Traducir.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Traducir.Location = new System.Drawing.Point(217, 69);
+            this.Traducir.Name = "Traducir";
+            this.Traducir.Size = new System.Drawing.Size(161, 32);
+            this.Traducir.TabIndex = 5;
+            this.Traducir.Text = "Ver";
+            this.Traducir.UseVisualStyleBackColor = false;
+            this.Traducir.Click += new System.EventHandler(this.Traducir_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(28, 107);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(552, 184);
+            this.listBox1.TabIndex = 6;
+            // 
+            // Diccionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 349);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Traducir);
+            this.Controls.Add(this.label1);
+            this.Name = "Diccionario";
+            this.Text = "Diccionario";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Traducir;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Diccionario/Diccionario.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Diccionario
+{
+    public partial class Diccionario : Form
+    {
+        //2 Hacer un programa que funcione como un diccionario, con palabra y definición, usando el Hashtable.
+        Hashtable diccionario = new Hashtable();
+
+        public Diccionario()
+        {
+            InitializeComponent();
+        }
+
+        private void Traducir_Click(object sender, EventArgs e)
+        {
+            diccionario.Add("Relación de afecto, simpatía y confianza que se establece entre personas que no son familia.", "1.Amistad");
+            diccionario.Add("Persona Que mantiene una relación de amistad con otra u otras personas. ", "2.Amigo");
+            diccionario.Add("Manera de estar colocadas las cosas o las personas en el espacio o de sucederse los hechos en el tiempo, según un determinado criterio o una determinada norma.", "3.Orden");
+            
+            foreach (string name in diccionario.Keys)
+            {
+                listBox1.Items.Add(diccionario[name] + "\n" + name);
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
